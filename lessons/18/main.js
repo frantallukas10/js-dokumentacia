8b088e9a453b6943c2e8fa1b4e0719c5874936bc
Ludmila Jalcova <lu.jalcova@gmail.com>
2019-07-17 18:32:04 +0200

    upload

---
+++ lessons/18/main.js
@@ -0,0 +1,16 @@
+// cyklus while
+
+let value1 = 1;
+
+while (value1 <= 10) {
+  console.log(`cislo je ${value1}`);
+  value1++;
+}
+
+// opacna logika
+let value2 = 10;
+
+while (value2 > 0) {
+  console.log(`cislo je ${value2}`);
+  value2--;
+}
